9ffa5de129e51980887dbf841a097119ed5db93d
GabrielJAllen <157070900+GabrielJAllen@users.noreply.github.com>
2025-10-14 19:56:26 +0000

    Steps 1 and 2 Completed using previous demo as a basis, also removed img files from src

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+
 import "./style.css";
 
+//deno - lint - ignore prefer -const
+let counter: number = 0;
+
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>Incremental Game</h1>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">🐐</button>
 `;
-//Trying to leave a comment, i cannot see what i am typing. hopefully this works :)
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = counter.toString();
+});
